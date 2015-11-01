e3157eaec1dec45c84ce0019923d1ec7f0def35a
Zach Schatz <zachschatz@gmail.com>
2015-11-01 14:45:34 -0500

    mode challenge completed

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -18,13 +18,42 @@
 
 
 # 1. Initial Solution
+=begin
+def mode (array)
+  hash = Hash.new
+  new_array = []
+  final_array = []
+  array.each do |x|
+    if hash[x] == nil
+        hash[x] = 0
+    else
+        hash[x] = hash[x] + 1
+    end
+  end
+  hash.each do |x,y|
+      new_array == []
+      new_array.push(y)
+    end
+    greatest_num = new_array.sort.last
+  hash.each do |x,y|
+    if y == greatest_num
+      final_array.push(x)
+    end
+  end
+  return final_array
+end
 
-
-
-
+=end
 # 3. Refactored Solution
 
-
+def mode (array)
+  hash = {}
+  final_array = []
+  array.each {|x| hash[x] = (hash[x] == nil ? 0 : hash[x] + 1)}
+  greatest_num = hash.values.max
+  hash.each {|x,y| final_array.push(x) if y == greatest_num}
+  return final_array
+end
 
 
 # 4. Reflection
