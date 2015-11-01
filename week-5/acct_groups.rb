9c10a8272c987807c897830b3ea36e3284c421be
Zach Schatz <zachschatz@gmail.com>
2015-11-01 17:37:00 -0500

    initial solution for acct_groups challenge

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -19,8 +19,46 @@
 
 # 1. Initial Solution
 
+=begin
+def group(names_list)
+  i = 0
+  j = i
+  cohort = []
+  squad = []
+  names_list.each do |x|
+    if i == 0
+      squad = []
+    end
 
+    squad.push(x)
 
+    i = i + 1
+    j = j + 1
+
+    if j == names_list.count
+      if i >= 3
+        cohort.push(squad)
+      else
+        person1 = cohort.last.pop
+        person2 = cohort.last.pop
+        squad.push(person1)
+        squad.push(person2)
+        cohort.push(squad)
+      end
+      return cohort
+    end
+
+    if i == 5
+      i = 0
+      cohort.push(squad)
+    end
+
+  end
+
+  return cohort
+
+end
+=end
 
 # 3. Refactored Solution
 
