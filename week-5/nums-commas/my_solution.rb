ce31118532129cc17c741c64af73d8e8a2a5c1b2
Zach Schatz <zachschatz@gmail.com>
2015-11-01 09:11:57 -0500

    setup for challenges

---
+++ week-5/nums-commas/my_solution.rb
@@ -0,0 +1,33 @@
+# Numbers to Commas Solo Challenge
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# 0. Pseudocode
+
+# What is the input? positive integer
+# What is the output? (i.e. What should the code return?) commas-separated integer as a string
+# What are the steps needed to solve the problem?
+# define condition to change integer to string
+  # integer_to.s
+# define condition to include commas
+  # IF integer.length > 3
+  # activate condition to include commas every 3 digital from the left
+
+# 1. Initial Solution
+def separate_comma (number)
+  if number < 4
+    p number.to_s
+
+
+
+
+# 2. Refactored Solution
+
+
+
+
+# 3. Reflection
