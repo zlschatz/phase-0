c58110417bbd6eb98c967adf81fb4e862b34bf65
Zach Schatz <zachschatz@gmail.com>
2015-11-01 23:49:11 -0500

    including reflections for challenges

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -12,10 +12,14 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? Array of integers / strings
+# What is the output? (i.e. What should the code return?) The value that appears the most
 # What are the steps needed to solve the problem?
-
+  # Define a hash
+  # Move values in the array to be keys in the hash
+  # Set values to the keys that will distinguish number of occurences
+  # Sort the keys to determine mode
+  # Return value attached to the key mode
 
 # 1. Initial Solution
 =begin
@@ -57,3 +61,21 @@ end
 
 
 # 4. Reflection
+=begin
+Which data structure did you and your pair decide to implement and why?
+
+I used a hash in order to count the number of times a value was presented in the array. The hash allows you to attached a value to any given key. Through this, the goal was to create a set of keys and find the mode.
+
+Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+
+This is definitely something that will take more practice. Going step by step makes sense, but I still find myself straying away from the pseudocode with other ideas. Sometimes it can be difficult to go back and revise the pseudocode while writing actual code.
+
+What issues/successes did you run into when translating your pseudocode to code?
+
+Determining the number of arrays / hashes that would be made seemed much easier at first. I ended up defining a few empty arrays at the start that I had not accounted for in my pseudocode.
+
+What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+
+I used .each, .sort, .last while writing the initial solution. These are all methods that I've implemented before. While refactoring, I learned about .values and was able to tighten up lines by removing do and if.
+
+=end
