9f43e65a1b6781f08ac948fe6d050848a8b98705
Zach Schatz <zachschatz@gmail.com>
2015-11-06 20:28:49 -0500

    completed pairing challenge nested_data_solution

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -68,3 +68,22 @@ startup_names.each do |inner_element|
 end
 
 #Reflection
+=begin
+
+What are some general rules you can apply to nested arrays?
+
+Here's an example of a nested array:
+nested_array = [1, ["inner", "array"], 2, 3]
+In this example, the nested array ["inner", "array"] has the index of 1. It is seen as one element within the nested_array. It can be returned by calling nested_array[1]. Elements inside the nested array can be selected with an additional index: nested_array[1][0].
+
+
+What are some ways you can iterate over nested arrays?
+
+You can iterate over nested arrays by making changes to elements within each array. This involves iterating further into deeper layers within the original array. In the end, you are able to manipulate each value - similarly to iterating through a typical array - by adding conditions. These conditions determine whether an an array exists within the current array level. This can become confusing, so it's best to be clear while labeling!
+
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+
+It took us longer than expected to complete the third release. We were using .each and returning the values - but not in an array. Then we tried using .collect with no success. After a while, we came across .map. Neither of us had used this before, but will definitely be sure to now! We also made .map destructive (!) to change the values of the original array.
+
+
+=end
